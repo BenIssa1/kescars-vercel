ab10bfac4ebe2c662136f64d45f8f22d44084e31
Ben Issa <issa23704@gmail.com>
2024-12-19 10:49:38 +0000

    Ok

--- middleware.ts
+++ middleware.ts
@@ -22,7 +22,7 @@ export default auth((req) => {
   }
 
   if (isAuthRoute) {
-    console.log(isLoggedIn, 'ddd')
+    console.log(isLoggedIn, 'Hello')
     if (isLoggedIn) {
       return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
     }
